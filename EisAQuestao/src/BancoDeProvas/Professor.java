c3905dee31c72e3725161d81e0e0bf89408d5d34
Leikode <20978729+Leikode@users.noreply.github.com>
2020-10-21 03:46:28 -0400

    Update Professor.java

--- EisAQuestao/src/BancoDeProvas/Professor.java
+++ EisAQuestao/src/BancoDeProvas/Professor.java
@@ -1,15 +1,23 @@
 package BancoDeProvas;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 public class Professor extends Pessoa implements Serializable {
     private String matricula;
 
     public Professor(){
         super();
-        this.nome = "nome";
         this.matricula = "matricula";
+    }
+
+    @Override
+    public String getNome() {
+        return super.nome;
+    }
+
+    @Override
+    public void setNome(String nome) {
+        super.nome = nome;
     }
 
     public String getMatricula() {
